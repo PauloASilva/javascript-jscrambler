795c54bf2688b0e021aa8d4b2123297cedf95ef5
José Magalhães <jose.magalhaes@jscrambler.com>
2016-06-01 12:53:43 +0100

    rebuild

--- dist/lib/config.js
+++ dist/lib/config.js
@@ -1,23 +1,22 @@
 'use strict';
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 var _rc = require('rc');
 
 var _rc2 = _interopRequireDefault(_rc);
 
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 // Load RC configuration if present. Pass `[]` as last argument to avoid
 // getting variables from `argv`.
-var config = (0, _rc2['default'])('jscrambler', {
+var config = (0, _rc2.default)('jscrambler', {
   keys: {},
   host: 'api4.jscrambler.com',
   port: 443
 }, []);
 
-exports['default'] = config;
-module.exports = exports['default'];
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9saWIvY29uZmlnLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7O2tCQUFlLElBQUk7Ozs7OztBQUluQixJQUFNLE1BQU0sR0FBRyxxQkFBRyxZQUFZLEVBQUU7QUFDOUIsTUFBSSxFQUFFLEVBQUU7QUFDUixNQUFJLEVBQUUscUJBQXFCO0FBQzNCLE1BQUksRUFBRSxHQUFHO0NBQ1YsRUFBRSxFQUFFLENBQUMsQ0FBQzs7cUJBRVEsTUFBTSIsImZpbGUiOiJjb25maWcuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgcmMgZnJvbSAncmMnO1xuXG4vLyBMb2FkIFJDIGNvbmZpZ3VyYXRpb24gaWYgcHJlc2VudC4gUGFzcyBgW11gIGFzIGxhc3QgYXJndW1lbnQgdG8gYXZvaWRcbi8vIGdldHRpbmcgdmFyaWFibGVzIGZyb20gYGFyZ3ZgLlxuY29uc3QgY29uZmlnID0gcmMoJ2pzY3JhbWJsZXInLCB7XG4gIGtleXM6IHt9LFxuICBob3N0OiAnYXBpNC5qc2NyYW1ibGVyLmNvbScsXG4gIHBvcnQ6IDQ0M1xufSwgW10pO1xuXG5leHBvcnQgZGVmYXVsdCBjb25maWc7XG4iXX0=
+exports.default = config;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9saWIvY29uZmlnLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBOzs7Ozs7OztBQUlBLElBQU0sU0FBUyxrQkFBRyxZQUFILEVBQWlCO0FBQzlCLFFBQU0sRUFEd0I7QUFFOUIsUUFBTSxxQkFGd0I7QUFHOUIsUUFBTTtBQUh3QixDQUFqQixFQUlaLEVBSlksQ0FBZjs7a0JBTWUsTSIsImZpbGUiOiJjb25maWcuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgcmMgZnJvbSAncmMnO1xuXG4vLyBMb2FkIFJDIGNvbmZpZ3VyYXRpb24gaWYgcHJlc2VudC4gUGFzcyBgW11gIGFzIGxhc3QgYXJndW1lbnQgdG8gYXZvaWRcbi8vIGdldHRpbmcgdmFyaWFibGVzIGZyb20gYGFyZ3ZgLlxuY29uc3QgY29uZmlnID0gcmMoJ2pzY3JhbWJsZXInLCB7XG4gIGtleXM6IHt9LFxuICBob3N0OiAnYXBpNC5qc2NyYW1ibGVyLmNvbScsXG4gIHBvcnQ6IDQ0M1xufSwgW10pO1xuXG5leHBvcnQgZGVmYXVsdCBjb25maWc7XG4iXX0=
